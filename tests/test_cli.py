b4458e57edc885433877f1aff0abda846639766b
Chris Bornmann <cbornman@ciena.com>
2024-06-23 13:56:14 -0700

    add test directory

---
+++ tests/test_cli.py
@@ -0,0 +1,5 @@
+
+# Do nothing...  This is just to satisfy py.test, which is being told to look for tests
+# in this directory.  I like tests in a "tests" directory under the actual source code,
+# not off on the side.
+pass
